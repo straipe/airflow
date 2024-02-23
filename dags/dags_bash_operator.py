d944a5e7d056ce8638baeebf1f0bbac716a88337
straipe <straipe@naver.com>
2024-02-23 11:56:13 +0900

    dags_bash_operator 수정

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_operator",
     schedule="0 0 * * *",
-    start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 2, 1, tz="Asia/Seoul"),
     catchup=False,
     params={"example_key": "example_value"},
 ) as dag:
