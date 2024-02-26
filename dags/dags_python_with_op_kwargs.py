36ab5d2defc8f2df5a198dc56c70c6d040ca4eb1
straipe <straipe@naver.com>
2024-02-26 10:21:37 +0900

    op_kwargs 실습

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+from common.common_func import regist, regist2
+
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2024, 2, 1, tz="Asia/Seoul"),
+    catch_up=False,
+) as dag:
+    regist_t2 = PythonOperator(
+        task_id="regist_t2",
+        python_callable=regist2,
+        op_args=["straipe", "man", "kr", "seoul"],
+        op_kwargs={"email": "straipe@naver.com", "phone": "010-6779-6060"},
+    )
