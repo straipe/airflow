52b50ed8ad946d5e16037e72b0265b264c26a518
straipe <straipe@naver.com>
2024-02-26 12:33:06 +0900

    data_interval 오탈자 수정

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -12,8 +12,8 @@ with DAG(
     bash_task_2 = BashOperator(
         task_id="bash_task_2",
         env={
-            "START_DATE": "{{ (date_interval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relatviedelta(days=19)) | ds }}",
-            "END_DATE": "{{ (date_interval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relatviedelta(days=14)) | ds }}",
+            "START_DATE": "{{ (data_interval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relatviedelta(days=19)) | ds }}",
+            "END_DATE": "{{ (data_interval_end.in_timezone('Asia/Seoul') - macros.dateutil.relativedelta.relatviedelta(days=14)) | ds }}",
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"',
     )
