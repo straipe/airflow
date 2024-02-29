1dd72f90768aebc973e84f8e6212d236d546906c
straipe <straipe@naver.com>
2024-02-29 11:22:33 +0900

    bash xcom dag_id 수정

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -4,7 +4,7 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_with_scom",
+    dag_id="dags_bash_with_xcom",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2024, 2, 1, tz="Asia/Seoul"),
     catchup=False,
