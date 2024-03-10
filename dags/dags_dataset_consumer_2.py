2d32f300110880c37e64c9a036a7ab868daf0aa4
straipe <straipe@naver.com>
2024-03-11 08:04:38 +0900

    consumer2 수정

--- dags/dags_dataset_consumer_2.py
+++ dags/dags_dataset_consumer_2.py
@@ -3,11 +3,12 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 import pendulum
 
+dataset_dags_dataset_producer_1 = Dataset("dags_dataset_producer_1")
 dataset_dags_dataset_producer_2 = Dataset("dags_dataset_producer_2")
 
 with DAG(
     dag_id="dags_dataset_consumer_2",
-    schedule=[dataset_dags_dataset_producer_2],
+    schedule=[dataset_dags_dataset_producer_1, dataset_dags_dataset_producer_2],
     start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
